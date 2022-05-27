b9c08d8dceeb1928f873fbc9b951ee0f8b9881a8
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 16:53:32 +0300

    AddCode

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -10,7 +10,20 @@ namespace Task8
     {
         static void Main(string[] args)
         {
+            int countOfGrannys;
+            int waitingTime = 10;
+            int hours=0;
+            int minutes=0;
+            int minutesInHour = 60;
 
+            Console.WriteLine("Введите количество старушек");
+            countOfGrannys = Int32.Parse(Console.ReadLine());
+            if(countOfGrannys > 0)
+            {
+                hours = (countOfGrannys * waitingTime) / minutesInHour;
+                minutes = (countOfGrannys * waitingTime) % minutesInHour;
+            }
+            Console.WriteLine("Вам нужно ждать "+hours+" часов 10"+minutes + " минут");
         }
     }
 }
